0fb5c3a5f5983f752f24ba96d54953ef99af1162
Abu Sayem Amir <sayem74@gmail.com>
2022-06-19 01:35:30 +0600

    completed assignment based on class 17

---
+++ admin/includes/navigation.php
@@ -0,0 +1,40 @@
+<!-- Main navigation -->
+					<div class="sidebar-category sidebar-category-visible">
+						<div class="category-content no-padding">
+						<ul class="navigation navigation-main navigation-accordion">
+
+								<!-- Main -->								
+								<li class="active"><a href="index.php"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
+								<li class=""><a href="bannerList.php"><i class="icon-image-compare"></i> <span>Banners</span></a></li>
+								<li class=""><a href="#"><i class="icon-home4"></i> <span>Services</span></a></li>
+								<li class=""><a href="#"><i class="icon-home4"></i> <span>Sections</span></a></li>
+								<li class=""><a href="#"><i class="icon-home4"></i> <span>Our Projects</span></a></li>
+								<li class=""><a href="#"><i class="icon-home4"></i> <span>Our Staff</span></a></li>
+								<li class=""><a href="#"><i class="icon-home4"></i> <span>Our Clients</span></a></li>
+								<li class=""><a href="#"><i class="icon-home4"></i> <span>Contact Message</span></a></li>
+								
+								<li>
+									<a href="#"><i class="icon-stack2"></i> <span>Back Office Setup</span></a>
+									<ul>
+										<li><a href="#">Category</a></li>									
+										<li><a href="#">Designation</a></li>									
+									</ul>
+								</li>							
+								<li>
+									<a href="#"><i class="icon-stack"></i> <span>Starter kit</span></a>
+										<ul>
+												<li>
+														<a href="#">3 columns</a>
+														<ul>
+															<li><a href="starters/3_col_dual.html">Dual sidebars</a></li>
+															<li><a href="starters/3_col_double.html">Double sidebars</a></li>
+														</ul>
+												</li>
+										</ul>
+								</li>
+							
+								<!-- /main -->
+						</ul>
+						</div>
+					</div>
+					<!-- /main navigation -->
